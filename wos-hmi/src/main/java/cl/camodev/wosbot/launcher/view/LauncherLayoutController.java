49b39c5bb9101921b509717ae0cbb1a8fc031c57
Neptune <anthony.g.ying@gmail.com>
2025-10-01 19:10:01 -0400

    Refactor LauncherLayoutController and TaskQueue for improved clarity and performance

--- wos-hmi/src/main/java/cl/camodev/wosbot/launcher/view/LauncherLayoutController.java
+++ wos-hmi/src/main/java/cl/camodev/wosbot/launcher/view/LauncherLayoutController.java
@@ -94,9 +94,7 @@ public class LauncherLayoutController implements IProfileLoadListener {
         initializeProfileComboBox();
         initializeModules();
         initializeExternalLibraries();
-        initializeEmulatorManager();
         showVersion();
-
     }
 
     private void showVersion() {
