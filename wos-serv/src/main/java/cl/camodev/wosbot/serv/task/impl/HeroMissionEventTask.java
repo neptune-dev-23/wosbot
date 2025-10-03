419937f6bea28874ae44dddf303a29293cfb358a
Neptune <anthony.g.ying@gmail.com>
2025-10-03 14:30:28 -0400

    Remove some unused imports, make my editor a bit happier
    # Conflicts:
    #	wos-utiles/src/main/java/cl/camodev/utiles/UtilOCR.java

--- wos-serv/src/main/java/cl/camodev/wosbot/serv/task/impl/HeroMissionEventTask.java
+++ wos-serv/src/main/java/cl/camodev/wosbot/serv/task/impl/HeroMissionEventTask.java
@@ -1,13 +1,6 @@
 package cl.camodev.wosbot.serv.task.impl;
 
-import cl.camodev.wosbot.almac.entity.DailyTask;
-import cl.camodev.wosbot.almac.repo.DailyTaskRepository;
-import cl.camodev.wosbot.almac.repo.IDailyTaskRepository;
-import cl.camodev.wosbot.console.enumerable.EnumConfigurationKey;
-import cl.camodev.wosbot.console.enumerable.EnumTemplates;
 import cl.camodev.wosbot.console.enumerable.TpDailyTaskEnum;
-import cl.camodev.wosbot.ot.DTOImageSearchResult;
-import cl.camodev.wosbot.ot.DTOPoint;
 import cl.camodev.wosbot.ot.DTOProfiles;
 import cl.camodev.wosbot.serv.task.DelayedTask;
 
