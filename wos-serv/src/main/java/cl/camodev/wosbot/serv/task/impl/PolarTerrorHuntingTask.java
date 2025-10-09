e38692ba1330a5772acbc8350446151145f2a69b
Davi <davibalieiro@gmail.com>
2025-10-09 15:10:08 +0100

    refactor: update methods calls and clean some code

--- wos-serv/src/main/java/cl/camodev/wosbot/serv/task/impl/PolarTerrorHuntingTask.java
+++ wos-serv/src/main/java/cl/camodev/wosbot/serv/task/impl/PolarTerrorHuntingTask.java
@@ -380,4 +380,9 @@ public class PolarTerrorHuntingTask extends DelayedTask {
         return EnumStartLocation.WORLD;
     }
 
+    @Override
+	protected boolean consumesStamina() {
+		return true;
+	}
+
 }
